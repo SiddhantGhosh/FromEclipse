13a47ee5c5ba0da627b71122899be0a70bb400c3
Siddhant Ghosh <Siddhant Ghosh@Siddhant>
2020-09-07 16:22:17 +0530

    First Commit from eclipse

---
+++ GitDemo/src/Add.java
@@ -0,0 +1,12 @@
+
+public class Add {
+
+	public int Addition(int a, int b) {
+
+		int result = a + b;
+
+		return result;
+
+	}
+
+}
---
+++ GitDemo/src/MainMethod.java
@@ -0,0 +1,13 @@
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+	
+		int a =1;
+		int b = 2;
+		Add obj = new Add();
+		
+		System.out.println("The sum is : "+obj.Addition(a, b));
+	}
+
+}
